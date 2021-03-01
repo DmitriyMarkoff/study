e32f514390f35e334d20697bde3a0d05e2bcc087
DmitriyMarkoff <dmitriy.markoff@gmail.com>
2021-03-01 19:46:04 +0200

    1

---
+++ solution1/solution1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.Write("Выберите задание (3, 6, 7): ");
+        switch (Console.ReadLine())
+        {
+            case "3":
+                {
+                    Console.WriteLine("Задание 3.");
+                    //Theme3();
+                    break;
+                }
+            case "6":
+                {
+                    Console.WriteLine("Задание 6.");
+                    //Theme6();
+                    break;
+                }
+            case "7":
+                {
+                    Console.WriteLine("Задание 7.");
+                    //Theme7();
+                    break;
+                }
+                
+        }
+        Console.ReadKey();
+        }
+    }
+}
