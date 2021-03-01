6b67d3f6148c07bc240d91d4d9869052ab5ba68c
DmitriyMarkoff <dmitriy.markoff@gmail.com>
2021-03-02 01:46:20 +0200

    task3

--- solution1/solution1/Program.cs
+++ solution1/solution1/Program.cs
@@ -5,33 +5,78 @@ using System.Text;
 
 namespace solution1
 {
+    class operation
+    {
+        public double x;
+        public double y;
+        public char z;
+        public void Show()
+        {
+            Console.WriteLine("Аргументы: ({0}, {1})", x, y);
+        }
+    }
+
+    class plus : operation
+    {
+        new public void Show()
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+    }
+    class minus : operation
+    {
+        new public void Show()
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
-        {   Console.Write("Выберите задание (3, 6, 7): ");
-        switch (Console.ReadLine())
         {
-            case "3":
-                {
-                    Console.WriteLine("Задание 3.");
-                    //Theme3();
-                    break;
-                }
-            case "6":
-                {
-                    Console.WriteLine("Задание 6.");
-                    //Theme6();
-                    break;
-                }
-            case "7":
-                {
-                    Console.WriteLine("Задание 7.");
-                    //Theme7();
-                    break;
-                }
-                
-        }
-        Console.ReadKey();
+            Console.Write("Выберите задание (3, 6, 7): ");
+            switch (Console.ReadLine())
+            {
+                case "3":
+                    {
+                        double arg1;
+                        double arg2;
+                        char oper;
+                        Console.Write("Задание 3.\nВВедите первый аргумент:");
+                        arg1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ВВедите второй аргумент:");
+                        arg2 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ВВедите знак операции (-,+):");
+                        oper = Convert.ToChar(Console.ReadLine());
+                        operation op = new operation { x = arg1, y = arg2 };
+                        op.Show();
+                        if (oper == '+')
+                        {
+                            plus op1 = new plus { x = arg1, y = arg2, z = oper };
+                            op1.Show();
+                        }
+                        else
+                        {
+                            minus op2 = new minus { x = arg1, y = arg2, z = oper };
+                            op2.Show();
+                        }
+                        break;
+                    }
+                case "6":
+                    {
+                        Console.WriteLine("Задание 6.");
+                        //paragraph6();
+                        break;
+                    }
+                case "7":
+                    {
+                        Console.WriteLine("Задание 7.");
+                        //paragraph7();
+                        break;
+                    }
+
+            }
+            Console.ReadKey();
         }
     }
 }
