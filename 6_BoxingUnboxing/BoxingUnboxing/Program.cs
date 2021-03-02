b2a940494e82c9ad6ef9f99b0bbbae4c3fe71f46
DmitriyMarkoff <dmitriy.markoff@gmail.com>
2021-03-02 22:01:04 +0200

    renamed

---
+++ 6_BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            passwords pass1 = new passwords { };
+            passwords pass2 = new passwords { };
+            Console.Write("Задание 6.\nВведите цифровой пароль: ");
+            pass1.symbols = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите символьный пароль: ");
+            pass2.symbols = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Ваш цифровой пароль увеличен вдвое: " + (int)pass1.symbols * 2);
+            Console.WriteLine("К Вашему символьному паролю добавлен символ: " + (string)pass2.symbols + 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6_BoxingUnboxing/BoxingUnboxing/passwords.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxingUnboxing
+{
+    class passwords
+    {
+        public object symbols { get; set; }
+    }
+}
